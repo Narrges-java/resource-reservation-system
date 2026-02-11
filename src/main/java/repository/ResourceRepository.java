85bb47e53644bed1d2ce57d297beb32376ec81d8
narges <narges.rahati004@gmail.com>
2026-02-11 15:47:30 +0330

    Initial project commit

---
+++ src/main/java/repository/ResourceRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceRepository extends JpaRepository<Resource,Long> {
+}
--- src/main/java/service/BookingService.java
+++ src/main/java/service/BookingService.java
@@ -1,4 +1,16 @@
 package service;
 
+import entity.Booking;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
 public interface BookingService {
+    Booking createBooking(Booking booking);
+
+    Booking cancelBooking (Booking booking);
+
+    List<Booking> findBookingsByDateRange(LocalDateTime startTime,LocalDateTime endTime);
+
+
 }
