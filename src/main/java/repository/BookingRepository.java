39943e069edc0e9194e765b3776475657d8a9e14
narges <narges.rahati004@gmail.com>
2026-02-18 16:43:39 +0330

    prevent of overlapping in Booking Resource.

--- src/main/java/repository/BookingRepository.java
+++ src/main/java/repository/BookingRepository.java
@@ -1,9 +1,29 @@
 package repository;
 
 import entity.Booking;
+import entity.enums.BookingType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+    @Query("""
+          SELECT b FROM Booking b
+          WHERE b.resource.id = :resourceId
+          AND b.status = :status
+          AND b.startTime < :endTime
+          AND b.endTime > :startTime
+""")
+    List<Booking> findOverlappingBooking(
+            @Param("resourceId") Long resourceId,
+            @Param("status") BookingType status,
+            @Param("startTime")LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime
+            );
 
 
 
--- src/main/java/service/impl/bookingServiceImpl.java
+++ src/main/java/service/impl/bookingServiceImpl.java
@@ -2,6 +2,8 @@ package service.impl;
 
 import entity.BaseEntity;
 import entity.Booking;
+import entity.enums.BookingType;
+import exception.BookingConflictException;
 import exception.InvalidBookingTimeException;
 import org.springframework.stereotype.Service;
 import repository.BookingRepository;
@@ -16,13 +18,31 @@ public class bookingServiceImpl implements BookingService {
     public bookingServiceImpl(BookingRepository bookingRepository) {
         this.bookingRepository = bookingRepository;
     }
+
+    // check TimeRange and compare is possible or not
     private void validateTimeRange(Booking booking) {
         if(!booking.getStartTime().isBefore( booking.getEndTime()) ){
             throw new InvalidBookingTimeException("Start time must be before end time");
         }}
+
+    // check overlap new booking with the oder booking(Reserved booking)
+    private void checkOverlap(Booking booking){
+        List<Booking> overlapping = bookingRepository.findOverlappingBooking(
+                booking.getResource().getId(),
+                BookingType.RESERVED,
+                booking.getStartTime(),
+                booking.getEndTime()
+        );
+        if (!overlapping.isEmpty()){
+            throw new BookingConflictException("Booking overlaps with existing reservations!");
+        }
+    }
+
+    // create Booking
     @Override
     public Booking createBooking(Booking booking){
         validateTimeRange(booking);
+        checkOverlap(booking);
         return bookingRepository.save(booking);
     }
 
